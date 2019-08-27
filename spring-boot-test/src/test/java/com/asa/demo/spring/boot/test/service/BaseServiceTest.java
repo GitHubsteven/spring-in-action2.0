3a6a6549a53592b622ab439b0d9047334a90201b
rongbin.xie <rongbin.xie@voyageone.cn>
2019-08-27 13:36:49 +0800

    add comment for the code

--- spring-boot-test/src/test/java/com/asa/demo/spring/boot/test/service/BaseServiceTest.java
+++ spring-boot-test/src/test/java/com/asa/demo/spring/boot/test/service/BaseServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
  * @Author jet.xie
- * @Description:
+ * @Description: this is the demo for the springBootTest for the test
  * @Date: Created at 10:54 2019/8/27.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
