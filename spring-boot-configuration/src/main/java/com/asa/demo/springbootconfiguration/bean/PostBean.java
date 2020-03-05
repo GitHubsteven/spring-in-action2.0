04f68a8b5cfa877b91a2b2bbe82331af0053e126
rongbin.xie <rongbin.xie@voyageone.cn>
2020-03-05 15:37:57 +0800

    增加spring boot读取http 参数的case

---
+++ spring-boot-configuration/src/main/java/com/asa/demo/springbootconfiguration/bean/PostBean.java
@@ -0,0 +1,23 @@
+package com.asa.demo.springbootconfiguration.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @version 1.0.0 COPYRIGHT © 2001 - 2019 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ * @Description:
+ * @Author asa.x
+ * @Date: Created at 15:09 2020/3/5.
+ */
+@Setter
+@Getter
+public class PostBean implements Serializable {
+    private String name;
+    private Integer age;
+    //服务器是否成功接收
+    private Boolean received;
+
+    private Object extendsInfo;
+}
--- spring-boot-configuration/src/main/java/com/asa/demo/springbootconfiguration/controller/DemoController.java
+++ spring-boot-configuration/src/main/java/com/asa/demo/springbootconfiguration/controller/DemoController.java
@@ -1,13 +1,12 @@
 package com.asa.demo.springbootconfiguration.controller;
 
 import com.asa.demo.springbootconfiguration.SpringBootConfigurationApplication;
+import com.asa.demo.springbootconfiguration.bean.PostBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,10 +27,39 @@ public class DemoController {
 
 
     @GetMapping("/configuration")
-    public Map<String, Object> getConfiguration() throws IOException {
+    public Map<String, Object> getConfiguration() {
         Map<String, Object> params = new HashMap<>();
         params.put("defaultUrl", defaultUrl);
         params.put("bizUrls", selfConfigProperties.getBiz());
         return params;
     }
+
+    @PostMapping("/requestBody")
+    public PostBean byRequestBody(@RequestBody PostBean params) {
+        params.setReceived(!StringUtils.isEmpty(params.getName()) && params.getAge() != null);
+        return params;
+    }
+
+    @PostMapping("/requestParameter")
+    public PostBean byRequestParameter(@RequestParam(value = "name", required = false) String name,
+                                       @RequestParam(value = "age", required = false) Integer age) {
+        PostBean response = new PostBean();
+        response.setName(name);
+        response.setAge(age);
+        response.setReceived(!StringUtils.isEmpty(name) && age != null);
+        return response;
+    }
+
+    @PostMapping("/together")
+    public PostBean together(@RequestParam(value = "name", required = false) String name,
+                             @RequestParam(value = "age", required = false) Integer age,
+                             @RequestBody PostBean request) {
+        PostBean response = new PostBean();
+        response.setName(name);
+        response.setAge(age);
+        response.setReceived(!StringUtils.isEmpty(name) && age != null);
+        response.setExtendsInfo(request);
+        return response;
+    }
+
 }
